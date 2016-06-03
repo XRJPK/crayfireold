8e795194a97d0294971f6a354653e71bb9128fa2
Sebastian Heldt <sebastianheldt@outlook.de>
2016-06-03 23:30:59 +0200

    datanbank ausbau

--- Gutachten GUI/files/lib/system/database/database.cs
+++ Gutachten GUI/files/lib/system/database/database.cs
@@ -8,7 +8,48 @@ namespace Gutachten_GUI.files.lib.system.database
 {
     abstract class Database
     {
-        // start initialization
-        
+
+        /**
+         * sql server hostname
+         * @var	string
+         */
+        protected string host = "";
+	
+       	/**
+    	 * sql server post
+    	 * @var	integer
+	     */
+	    protected int port = 0;
+
+        /**
+         * sql server login name
+         * @var	string
+         */
+        protected string user = "";
+
+        /**
+         * sql server login password
+         * @var	string
+         */
+        protected string password = "";
+	
+    	/**
+	     * database name
+	    * @var	string
+	    */
+	    protected string database = "";
+
+
+        /**
+         * enables failsafe connection
+         * @var	boolean
+         */
+        protected bool failsafeTest = false;
+	
+    	/**
+    	 * number of executed queries
+    	 * @var	integer
+    	 */
+    	protected int queryCount = 0;
     }
 }
